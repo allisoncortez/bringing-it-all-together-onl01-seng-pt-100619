f1b46dd70adffa2ff9f8701ce7a6b76ee726c926
allison cortez <allizetorc@gmail.com>
2019-12-23 03:12:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,8 +90,8 @@ class Dog
   end 
   
   def update
-    # sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    # DB[:conn].execute(sql, self.name, self.grade, self.id)
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
   
 end
